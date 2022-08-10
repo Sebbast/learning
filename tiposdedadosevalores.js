362ef260ae9c3d64d0d9b472d1f99cfbb0df2e31
Sebbast <silvarafa2008@hotmail.com>
2022-08-10 15:52:19 -0300

    objeto em objeto

--- tiposdedadosevalores.js
+++ tiposdedadosevalores.js
@@ -8,3 +8,14 @@ const cliente = {
 
 cliente.fones.forEach(fone => console.log(fone));/*acessou o dado fones da const cliente, "fones" é uma array, dessa forma podemos usar qualquer metodo de array que quisermos
 ao acessar os dados desejados no clientes.fones.metodo*/
+
+//objetos em objetos
+
+cliente.dependentes = {
+    nome: "Cleiberson",
+    parentesco: "Filho",
+    nascimento: "24/12/0001"
+}
+
+cliente.dependentes.nome = "Cleiberson Marcolas";
+;console.log(cliente);
