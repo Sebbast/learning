3ea3d3fac5b6d9bf09c460e4c4fd9a8590201e44
Sebbast <silvarafa2008@hotmail.com>
2022-08-10 15:26:56 -0300

    new

---
+++ tiposdedadosevalores.js
@@ -0,0 +1,10 @@
+const cliente = {
+    nome: "Andre",
+    idade: 36,
+    cpf: "77777777777777",
+    email: "@email.com",
+    fones: ["8794564", "12345678"]
+}
+
+cliente.fones.forEach(fone => console.log(fone));/*acessou o dado fones da const cliente, "fones" é uma array, dessa forma podemos usar qualquer metodo de array que quisermos
+ao acessar os dados desejados no clientes.fones.metodo*/
